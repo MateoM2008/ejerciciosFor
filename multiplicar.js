cb175500fede4790a28b793a415cf1ba3fa6a8db
MateoM2008 <mateomunozlema000@gmail.com>
2026-07-14 17:25:54 -0500

    mejorar la interfaz para todos los usuarios

--- multiplicar.js
+++ multiplicar.js
@@ -1,9 +1,14 @@
-function generarTablas(){
-    let tabla = 5;
+function generarTablas() {
+    
     let contenido = "";
+    let cmpNumero = document.getElementById("txtTabla")
+    let strNumero = cmpNumero.value;
+    let numero=parseInt(strNumero);
+
+    let tabla = numero;
 
     let contenedor = document.getElementById("tblResultado");
-    for(let i = 1; i <= 10; i++){
+    for (let i = 1; i <= 10; i++) {
 
         contenido += `
             <tr>
